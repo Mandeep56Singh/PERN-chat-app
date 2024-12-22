ed3584525d000d48fca0980bbe17a8065ec33950
mandeepSingh <mandeepraj2312@gmail.com>
2024-12-22 16:27:14 +0530

    feat: add signup feature

---
+++ Back End/src/controllers/auth.controller.ts
@@ -0,0 +1,55 @@
+import bcryptjs from "bcryptjs";
+import { error } from "console";
+import { Request, Response } from "express";
+import prisma from "../db/prisma";
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: "Please fill in all fields" });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Password doesn't match" });
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (user) {
+      return res.send(400).json({ error: "Username already exists" });
+    }
+
+    // Hashing the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender == "male" ? boyProfilePic : girlProfilePic,
+      },
+    });
+
+    if (newUser) {
+      // generate new token in a sec
+
+      res.status(201).json({
+        id: newUser.id,
+        fullName: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "invalid user data" });
+    }
+  } catch (error: any) {
+    console.log("Error in signup", error.message);
+  }
+};
--- Back End/src/index.ts
+++ Back End/src/index.ts
@@ -3,9 +3,13 @@ import authRoutes from './routes/auth.routes'
 import messageRoutes from './routes/message.route'
 const app = express();
 
+app.use(express.json())
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
+app.get("/",(req, res) => {
+    res.send("Hello world")
+})
 app.listen(5000, () => {
     console.log("Server is running on Port:")
 })
